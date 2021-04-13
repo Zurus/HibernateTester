3143536b8e547c8c55b00117d0a5d126a16fdff6
ADivaev <ADivaev@bellintegrator.com>
2021-04-13 13:21:29 +0500

    init commit

---
+++ src/main/java/com/javasampleapproach/jpa/one2many/karaulova/Static.java
@@ -0,0 +1,13 @@
+package com.javasampleapproach.jpa.one2many.karaulova;
+
+/**
+ * Static.
+ *
+ * @author ADivaev
+ */
+public class Static {
+    public final static String EXIT_COMMAND = "exit";
+    public final static String SAVE_COMMAND = "save";
+    public final static String DEL_COMMAND = "delete";
+    public final static String SHOW_COMMAND = "show";
+}
